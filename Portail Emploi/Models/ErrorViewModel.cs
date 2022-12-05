de206ca8b0ec827f1d9beb5ddd23101ba318e8b4
Jaouad-Said <jaouad_sd@hotmail.com>
2022-12-05 15:11:41 +0100

    Ajoutez des fichiers projet.

---
+++ Portail Emploi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portail_Emploi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
